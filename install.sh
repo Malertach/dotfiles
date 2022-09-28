a970279960c901f7eb25ce99d0a5290ca8297378
Malertach <>
2022-09-28 19:36:58 +0200

    fix only backing up symlinks

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ mkdir ~/.dotfiles_bak.d
 echo "Installing DotFiles"
 for file in ${FILES[*]}; do
     # make backup, if no backup exists already
-    cp -Ln ~/"$file" ~/dotfiles_bak.d/"$file"
+    cp -n ~/"$file" ~/dotfiles_bak.d/"$file"
     # remove old dotfile
     rm -rf ~/"$file"
     # insert new dotfile
